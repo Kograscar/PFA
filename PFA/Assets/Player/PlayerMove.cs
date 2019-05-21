a6fa2ee02beb73bc2c1fc6546973834b018a4e5b
DorianVENTURA <50734507+DorianVENTURA@users.noreply.github.com>
2019-05-21 09:33:31 +0200

    Player_New

---
+++ PFA/Assets/Player/PlayerMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] private string horizontalInputName;
+    [SerializeField] private string verticalInputName;
+    [SerializeField] private float moveSpeed;
+
+    private CharacterController charController;
+
+    private void Awake()
+    {
+        charController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        PlayerMovement();
+    }
+
+    private void PlayerMovement()
+    {
+        float horizInput = Input.GetAxis(horizontalInputName) * moveSpeed * Time.deltaTime;
+        float verInput = Input.GetAxis(verticalInputName) * moveSpeed * Time.deltaTime;
+
+        Vector3 forwardMovement = transform.forward * verInput;
+        Vector3 rightMovement = transform.right * horizInput;
+
+        charController.SimpleMove(forwardMovement + rightMovement);
+    }
+}

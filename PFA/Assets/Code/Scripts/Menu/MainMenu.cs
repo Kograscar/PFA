69a612af38d0f14382b279fda58c39ee75cbe6af
Kograscar <2lcramer@gmail.com>
2019-05-29 13:50:13 +0200

    14h | Léopold | 29 May 2019

---
+++ PFA/Assets/Code/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    //[SerializeField] string _gameScene;
+    [SerializeField] Scene _gameScene;
+    [SerializeField] AudioMixer audioMixer;
+    [SerializeField]public GameObject _playButton;
+
+    void Start(){
+        //_playButton.GetComponent<Button>().
+    }
+    
+   public void Playgame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame ()
+    {
+        Debug.Log("Quitter le jeu");
+        Application.Quit();
+    }
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("MainVolume", volume);
+    }
+}

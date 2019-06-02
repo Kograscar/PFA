3c0e6d8e28b4cf72e85c812d01b8ce717dbf30a7
Dunalastair <50734416+Dunalastair@users.noreply.github.com>
2019-06-02 08:12:10 +0200

    8h | 2 june 2019 | Jérémy
    SoundScripts

---
+++ PFA/Assets/Scenes/JEJE/Footsteps.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Footsteps : MonoBehaviour {
+
+	CharacterController cc;
+	public AudioSource SourceAudio ;
+
+	void Start ()	
+ 	{
+		cc = GetComponent<CharacterController>();
+        GetComponent<AudioSource>();
+
+    }
+	
+	void Update ()	
+ 	{
+		if(cc.isGrounded == true && cc.velocity.magnitude > 2f && GetComponent<AudioSource>().isPlaying == false)
+		{
+            GetComponent<AudioSource>().volume = Random.Range(0.8f, 1);
+            GetComponent<AudioSource>().pitch = Random.Range(0.8f, 1.1f);
+            GetComponent<AudioSource>().Play();
+        }
+	}
+}

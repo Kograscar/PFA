587a3bcec9ba2dcd01a4a1e224f6127264e03d45
Dorian Ventura <dorianventura.gd@gmail.com>
2019-06-05 02:42:50 +0200

    Add: IA Proto ?!

---
+++ PFA/Assets/Code/IA_Dorian_Try/RandomPosition.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomPosition : MonoBehaviour
+{
+    public Vector3[] randomPosition;
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Ghost")
+        {
+            int randomNumber = Random.Range(0, randomPosition.Length);
+            transform.position = randomPosition[randomNumber]; 
+        }
+    }
+}

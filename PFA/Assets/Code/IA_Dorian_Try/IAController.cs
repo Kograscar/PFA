f035f81d4911d9938dcae7ad29b7dab3ef82d10c
Dorian Ventura <dorianventura.gd@gmail.com>
2019-06-06 13:27:44 +0200

    add: New for a better start

--- PFA/Assets/Code/IA_Dorian_Try/IAController.cs
+++ PFA/Assets/Code/IA_Dorian_Try/IAController.cs
@@ -5,61 +5,84 @@ using UnityEngine.AI;
 
 public class IAController : MonoBehaviour
 {
+    private enum State { GotoRandomPoint, ChasePlayer }
+
     public float lookRadius = 5f;
+    public float maxRadius = 10f;
+    public float maxAngle = 30f;
     public Transform randomPoint;
 
-    Transform target;
-    NavMeshAgent agent;
+    Transform _target;
+    NavMeshAgent _agent;
+    State _state = State.GotoRandomPoint;
 
     void Start()
     {
-        target = PlayerManager.instance.Player.transform;
-        agent = GetComponent<NavMeshAgent>();
+        _target = PlayerManager.instance.Player.transform;
+        _agent = GetComponent<NavMeshAgent>();
     }
 
     void Update()
     {
-        float distance = Vector3.Distance(target.position, transform.position);
-        float distanceRandomPoint = Vector3.Distance(randomPoint.position, transform.position);
+        SetState();
+        ReactToState();
+    }
+
+    void SetState()
+    {
+        float distance = Vector3.Distance(_target.position, transform.position);
 
         //On créer un champ de vision face a l'ennemi...
-        Vector3 direction = target.position - this.transform.position;
-        float angle = Vector3.Angle(direction, this.transform.forward);
-        if (Vector3.Distance(target.position, this.transform.position) < 10 && angle < 30)
+        Vector3 direction = _target.position - transform.position;
+        float angle = Vector3.Angle(direction, transform.forward);
+
+        bool farDetection = angle < maxAngle && distance < maxRadius;
+        bool nearDetection  = distance <= lookRadius;
+
+        if (nearDetection || farDetection)
         {
-            print("CA MARCHE");
-            agent.SetDestination(target.position);
+            _state = State.ChasePlayer;
         }
-
-        //On dit a l'ennemi d'aller au randomPoint...
-        if (distance > lookRadius && Vector3.Distance(target.position, this.transform.position) > 10 && angle > 30)
+        else
         {
-            agent.SetDestination(randomPoint.position);
+            _state = State.GotoRandomPoint;
         }
+    }
 
-        //On dit a l'ennemi de traquer le joueur et de l'attaquer...
-        if (distance <= lookRadius)
+    void ReactToState()
+    {
+        Debug.Log("Le state est " + _state);
+
+        float distance = Vector3.Distance(_target.position, transform.position);
+
+        switch (_state)
         {
-            agent.SetDestination(target.position);
+            case State.GotoRandomPoint:
+                _agent.SetDestination(randomPoint.position);
+                break;
 
-            if (distance <= agent.stoppingDistance)
-            {
-                print("attack");
-            }
+            case State.ChasePlayer:
+                _agent.SetDestination(_target.position);
+
+                if (distance <= _agent.stoppingDistance)
+                {
+                    print("attack");
+                }
+                break;
         }
-
     }
 
     void FaceTaget()
     {
-        Vector3 direction = (target.position - transform.position).normalized;
+        Vector3 direction = (_target.position - transform.position).normalized;
         Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
         transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
     }
 
-    void OnDrawGizmosSelected()
+    void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere (transform.position, lookRadius);
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+        Gizmos.DrawWireSphere(transform.position, maxRadius);
     }
 }
--- PFA/Assets/Code/IA_Dorian_Try/RandomPosition.cs
+++ PFA/Assets/Code/IA_Dorian_Try/RandomPosition.cs
@@ -4,16 +4,42 @@ using UnityEngine;
 
 public class RandomPosition : MonoBehaviour
 {
-     [SerializeField] Vector3[] _randomPosition;
+    public readonly static int EXIT_INDEX_VALUE = 5; // constante chelou tqt
+
+
+    [SerializeField] Vector3[] _randomPosition;
 
     private void OnTriggerEnter(Collider collider)
     {
         if (collider.gameObject.tag == "Ghost")
         {
-            print("yes");
-            int randomNumber = Random.Range(0, _randomPosition.Length);
-            transform.position = _randomPosition[randomNumber];
-
+            Debug.Log("OnTriggerEnter");
+            transform.position = GenerateRandomPosition();
         }
+    }
+
+    // au cas où wola
+    private void OnTriggerStay(Collider collider)
+    {
+        if (collider.gameObject.tag == "Ghost")
+        {
+            Debug.Log("OnTriggerStay");
+            transform.position = GenerateRandomPosition();
+        }
+    }
+
+    Vector3 GenerateRandomPosition()
+    {
+        int exitIndex = 0;
+        int randomNumber = 0;
+
+        do
+        {
+            exitIndex++;
+            randomNumber = Random.Range(0, _randomPosition.Length);
+        }
+        while (_randomPosition[randomNumber] == transform.position && exitIndex < EXIT_INDEX_VALUE);
+
+        return _randomPosition[randomNumber];
     }
 }

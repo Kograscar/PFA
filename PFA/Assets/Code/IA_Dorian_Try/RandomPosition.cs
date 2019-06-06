a25a8faf4762b233651318a299a3420f28b1e9c6
Dorian Ventura <dorianventura.gd@gmail.com>
2019-06-06 09:26:30 +0200

    Add: Script IA for Leopold

--- PFA/Assets/Code/IA_Dorian_Try/RandomPosition.cs
+++ PFA/Assets/Code/IA_Dorian_Try/RandomPosition.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class RandomPosition : MonoBehaviour
 {
-    public Vector3[] randomPosition;
+     [SerializeField] Vector3[] _randomPosition;
 
     private void OnTriggerEnter(Collider collider)
     {
         if (collider.gameObject.tag == "Ghost")
         {
             print("yes");
-            int randomNumber = Random.Range(0, randomPosition.Length);
-            transform.position = randomPosition[randomNumber]; 
+            int randomNumber = Random.Range(0, _randomPosition.Length);
+            transform.position = _randomPosition[randomNumber];
+
         }
     }
 }

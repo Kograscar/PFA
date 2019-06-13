3030dc34006f9c22de973c74e672668475bf3710
Kograscar <2lcramer@gmail.com>
2019-06-13 18:26:46 +0200

    Add : Victory Screen System

---
+++ PFA/Assets/Code/Scripts/InteractableItem/ShowVictoryScreen.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowVictoryScreen : MonoBehaviour
+{
+    public int _milestones;
+    [SerializeField] GameObject _victoryScreen;
+
+
+    void Update()
+    {
+        if(_milestones >= 2){
+            _victoryScreen.SetActive(true);
+            GameObject.FindObjectOfType<CharController>().gameObject.SetActive(false);
+            gameObject.SetActive(false);
+        }
+    }
+}
